0956180aa5bdea7adb6a6e8540872a12df2ff320
jager <ondeground@gmail.com>
2015-06-05 21:49:55 -0500

    get data
    additions to the scripts: the code to download and unzip the data file

--- plot1.R
+++ plot1.R
@@ -1,4 +1,7 @@
 plot1<-function() {
+        zipfile="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(zipfile,destfile="./house.zip",method="curl")
+        unzip("house.zip",files="household_power_consumption.txt")
         project<-read.table("household_power_consumption.txt",sep=";",skip=66637,nrows=2880)
         variablenames<-read.table("household_power_consumption.txt",header=T,sep=";",nrows=1)
         colnames(project)<-names(variablenames)
@@ -6,5 +9,4 @@ plot1<-function() {
         png(filename = "plot1.png",width=480,height=480,units="px")
         hist(project$Global_active_power,col="red",xlab= "Global Active Power (kilowatts)",main="Global Active Power")
         dev.off()
-
 }
--- plot2.R
+++ plot2.R
@@ -1,4 +1,7 @@
 plot2<-function() {
+        zipfile="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(zipfile,destfile="./house.zip",method="curl")
+        unzip("house.zip",files="household_power_consumption.txt")
         project<-read.table("household_power_consumption.txt",sep=";",skip=66637,nrows=2880)
         variablenames<-read.table("household_power_consumption.txt",header=T,sep=";",nrows=1)
         colnames(project)<-names(variablenames)
